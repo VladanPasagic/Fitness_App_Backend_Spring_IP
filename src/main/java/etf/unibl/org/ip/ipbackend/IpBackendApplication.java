6b79ac25ce5ebe6594fab7aba0cf0ea66dcd4e92
Pasha <vladanpasagic01@gmail.com>
2024-03-18 23:32:46 +0100

    Init of spring application
    Initial setup of spring application

---
+++ src/main/java/etf/unibl/org/ip/ipbackend/IpBackendApplication.java
@@ -0,0 +1,13 @@
+package etf.unibl.org.ip.ipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/etf/unibl/org/ip/ipbackend/IpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package etf.unibl.org.ip.ipbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
